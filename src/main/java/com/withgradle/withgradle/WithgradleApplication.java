f537183043727573863cd0074da22982e7b1f8d0
waqasdilawar <waqas.dilawar@discorp.com>
2018-12-05 14:31:41 +0500

    Added project files

---
+++ src/main/java/com/withgradle/withgradle/WithgradleApplication.java
@@ -0,0 +1,12 @@
+package com.withgradle.withgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithgradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithgradleApplication.class, args);
+	}
+}
